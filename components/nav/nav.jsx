5cf499393843d450662334bfd6f3c1e1626fbcc7
Mit Sananikone <Mitsananikone@gmail.com>
2023-09-21 11:23:48 -0500

    Navbar hamburger animates in and out

--- components/nav/nav.jsx
+++ components/nav/nav.jsx
@@ -3,9 +3,9 @@
 import { useState, useRef } from "react";
 import Link from "next/link";
 import styles from "./nav.module.css";
-import ContactFire from '@/components/nav/contactFire/contactFire'; 
-import HamburgerIcon from './hamburgerIcon/hamburgerIcon';
-// import "bootstrap/dist/css/bootstrap.min.css";
+import ContactFire from "@/components/nav/contactFire/contactFire";
+import HamburgerIcon from "./hamburgerIcon/hamburgerIcon";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,26 +18,40 @@ const Navbar = () => {
     { name: "QUOTE", link: "/quote/quotePage" },
     { name: "PRICING", link: "/pricing/pricingPage" },
     { name: "PROMO", link: "/promo/promoPage" },
-    { name: "CONTACTS", link: "/contacts/contactsPage" }, 
+    { name: "CONTACTS", link: "/contacts/contactsPage" },
   ];
 
-  const toggleMenu = () => {
-    if (isMenuOpen) {
-      if (navItemHamburgerRef.current) {
-        navItemHamburgerRef.current.classList.add(styles.closing);
-      }
-      setTimeout(() => {
-        setIsMenuOpen(false);
-      }, 2000); 
-    } else {
-      setIsMenuOpen(true);
-      if (navItemHamburgerRef.current) {
-        navItemHamburgerRef.current.classList.remove(styles.closing);
-      }
+  const openMenu = () => {
+    setIsMenuOpen(true);
+    if (navItemHamburgerRef.current) {
+      navItemHamburgerRef.current.classList.add(styles.opening);
     }
   };
 
+  const closeMenu = () => {
+    if (navItemHamburgerRef.current) {
+      navItemHamburgerRef.current.classList.add(styles.closing);
+      navItemHamburgerRef.current.classList.remove(styles.opening);
+      // Get total animation time
+      const animationDuration = 0.5; // seconds
+      const totalItems = menuItems.length;
+      const animationDelay = 0.5; // seconds
+      const totalAnimationTime =
+        animationDuration + animationDelay * (totalItems - 1);
+        
+      setTimeout(() => {
+        setIsMenuOpen(false);
+        navItemHamburgerRef.current.classList.remove(styles.closing);
+        navItemHamburgerRef.current
+          .querySelectorAll(styles.title)
+          .forEach((item) => (item.style.opacity = "1"));
+      }, totalAnimationTime * 1000);
+    }
+  };
 
+  const toggleMenu = () => {
+    isMenuOpen ? closeMenu() : openMenu();
+  };
 
   return (
     <nav className={styles.nav}>
@@ -49,65 +63,43 @@ const Navbar = () => {
             className="logo cursor-pointer"
           />
         </Link>
+
+        {/* Regular Menu Items (For Desktop) */}
         <div className={styles.hamburger}>
           {menuItems.map((item, index) => (
-            <Link
-              href={item.link || `#${item.name?.toLowerCase() || item.toLowerCase()}`}
-              key={index}
-              passHref
-            >
-               <span
-      className={styles.navItem}
-      onMouseOver={(e) => {
-        e.target.style.textShadow = "2px 2px 4px #2119B4";
-      }}
-      onMouseOut={(e) => {
-        e.target.style.textShadow = "1px 1px 2px rgba(0,0,0,0.3)";
-      }}
-    >
-      {item.name === "CONTACTS" ? <ContactFire /> : item.name}
-    </span>
+            <Link href={item.link} key={index} passHref>
+              <span className={styles.navItem}>
+                {item.name === "CONTACTS" ? <ContactFire /> : item.name}
+              </span>
             </Link>
           ))}
         </div>
 
-       <button className={styles.hamburgerButton}>
-       <HamburgerIcon toggleMenu={toggleMenu} />
+        {/* Hamburger Icon */}
+        <button className={styles.hamburgerButton} onClick={toggleMenu}>
+    <HamburgerIcon toggleMenu={toggleMenu} />
 </button>
-
       </div>
+
+      {/* Mobile Menu Items (Inside Modal) */}
       {isMenuOpen && (
-         <div 
-         ref={navItemHamburgerRef} 
-         className={isMenuOpen ? `${styles.navItemHamburger} ${styles.open}` : styles.navItemHamburger}>
-         {menuItems.map((item, index) => (
-           <Link
-             key={index}
-             href={item.link || `#${item.name?.toLowerCase() || item.toLowerCase()}`}
-             passHref
-           >
-             <span
-    className={styles.navItem}
-    style={{ 
-      animationDelay: `${index * 0.15}s`,
-      '--index': index
-    }}
-               onMouseOver={(e) => {
-                 e.target.style.textShadow = "2px 2px 4px #2119B4";
-               }}
-               onMouseOut={(e) => {
-                 e.target.style.textShadow = "1px 1px 2px rgba(0,0,0,0.3)";
-               }}
-             >
-              <div className={styles.title} >
-              <h6 >
-              {item.name === "CONTACTS" ? <ContactFire /> : item.name}
-              </h6>
-               </div>
-             </span>
-           </Link>
-         ))}
-       </div>
+        <div
+          ref={navItemHamburgerRef}
+          className={`${styles.navItemHamburger} ${
+            isMenuOpen ? styles.opening : styles.closing
+          }`}
+        >
+          {menuItems.map((item, index) => (
+            <Link href={item.link} key={index} passHref>
+              <div
+                className={styles.title}
+                style={{ "--index": index, "--total-items": menuItems.length }}
+              >
+                {item.name === "CONTACTS" ? <ContactFire /> : item.name}
+              </div>
+            </Link>
+          ))}
+        </div>
       )}
     </nav>
   );
