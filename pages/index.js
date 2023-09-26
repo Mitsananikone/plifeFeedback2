66e9e9a10c58d8366dc74efe3ab3fec2bd0466bd
Mit Sananikone <Mitsananikone@gmail.com>
2023-09-26 17:58:37 -0500

    Landing page popup implemented

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import LandingPage from '@/pages/home/landingpage'
-import LandingPopup from '@/components/home/landingPopup/landingPopup';
+import LandingPopup from '@/components/landingPopup/landingPopup';
 
-import FAQPage from '@/pages/faq/faqPage';
-import PricingPage from '@/pages/pricing/pricingPage';
 
 export default function Home() {
   return (
     <>
-       <LandingPopup /> 
-      <div className="screenSize">
+         <LandingPopup />
+      {/* <div className="screenSize"> */}
         <div className="full-flex-centered">
-
-           <LandingPage /> 
-
-        </div>
+     
+          <LandingPage />
+{/* <PricingPage/> */}
+        {/* <FAQPage/> */}
+        {/* </div> */}
       </div>
 
     </>
